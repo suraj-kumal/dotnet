df08a8b54fbbd320eb3f226802ba66f2cc716324
zerocool <89600461+suraj-kumal@users.noreply.github.com>
2024-02-28 00:03:17 +0545

    c#

--- Array.cs
+++ Array.cs
@@ -1,8 +1,11 @@
 using System;
  namespace NCClabsuraj{
     class Even{
+        //array delcare
+        //[] empty means can be set letter
         private int[] num;
         public Even(){
+            //logic to store even number between 0 to 30
             num = new int[15];
             int i;
             int index=0;
@@ -13,6 +16,8 @@ using System;
                 }
             }
         }
+
+        //display even numbers
         public void Display(){
             for(int i =0;i<num.Length;i++){
                 Console.WriteLine($"index of {i} = "+num[i]);
@@ -21,7 +26,7 @@ using System;
         }
     }
     internal class Array{
-        public static void Main(string[] args)
+        public static void Main()
         {
                 Even num = new Even();
                 num.Display();
